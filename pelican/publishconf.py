bf898a3dc519d7a955ae6e24968bb72916c6c9c5
40423238 <40423238@gm.nfu.edu.tw>
2016-01-15 19:53:50 +0800

    40423238

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423238 GitHub', 'http://40423238.github.io/CPB_40423238/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423238 個人網頁', 'http://40423238.github.io/CPB_40423238/'))
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('github', 'http://github.com/ametaireau'),)
